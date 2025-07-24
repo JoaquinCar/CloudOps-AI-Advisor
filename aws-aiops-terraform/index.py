f959cd1c5fdb9284ad1968825d6c4e5490c5cdd5
JoaquinCar <joaquincarmona758@gmail.com>
2025-07-23 23:30:20 -0600

    lambda to test high metrics

--- aws-aiops-terraform/index.py
+++ aws-aiops-terraform/index.py
@@ -1,5 +1,30 @@
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Hola desde Lambda!"
-    }
+    try:
+        metric = float(event.get("metric", 0.5))  # Valor por defecto: 0.5
+        logger.info(f"Received metric: {metric}") #para que se vea en los logs de cloudwatch
+
+        if metric > 0.8:
+            logger.warning("⚠️ High value detected! Possible anomaly.")
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"alert": True, "value": metric})
+            }
+        else:
+            logger.info("✅ Normal value") #para que se vea en los logs de cloudwatch
+            return {
+                "statusCode": 200,
+                "body": json.dumps({"alert": False, "value": metric})
+            }
+
+    except Exception as e:
+        logger.error(f"Error processing event: {str(e)}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
